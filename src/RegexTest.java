1d677bade4c22ff0541f758e59bb63173eae7c4e
Nanne Wielinga <--global>
2016-04-26 19:13:19 +0200

    recognise strings

--- src/RegexTest.java
+++ src/RegexTest.java
@@ -63,5 +63,20 @@ public class RegexTest {
         RegexTest FLOAT = new RegexTest(String.format("%s|(%s|%s)", UnsignedInt, UnsignedReal1, UnsignedReal2));
         //FLOAT.runTest("2346", 0);
         FLOAT.runTest("0.6", 0);
+        //language=RegExp
+        String rString = "\"(([\\\\][\"])|[^\"])*\"";
+
+        rString = rString.replace("\"", "\\\"");
+
+        RegexTest STRING = new RegexTest(rString);
+//        STRING.runTest("'te'st'", 0);
+//        STRING.runTest("'te\\'st'", 0);
+        STRING.runTest("\"te\"st\"", 0);
+        STRING.runTest("\"te\\\"st\"", 0);
+        STRING.runTest("\"test\"", 0);
+        STRING.runTest("\"test", 0);
+        STRING.runTest("test", 0);
+//        STRING.runTest("opblaasboot", 0);
+
     }
 }
