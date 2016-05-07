60acbb7ee37b7fd80a68195b325448b268ba21fc
LuukvH <laj@home.nl>
2016-05-07 15:02:33 +0200

    Changed tokens

---
+++ src/enums/Token.java
@@ -0,0 +1,28 @@
+package enums;
+
+/**
+ * Created by laj on 4-5-2016.
+ */
+public enum Token {
+    k_begin,
+    k_declare,
+    k_end,
+
+    semi_colon,
+    comma,
+    bar,
+    lparen,
+    rparen,
+    minus,
+    times,
+    plus,
+
+    id,
+    nat,
+
+    identifier,
+    assign,
+    error,
+    end_of_input
+
+}
